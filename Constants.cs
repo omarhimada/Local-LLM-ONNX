4625716d3685eb53f61f96f717693fd3a00bb197
iAdos <iAdos@MY>
2026-06-11 12:20:16 -0400

    model copy

--- Constants.cs
+++ Constants.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.AI;
 namespace OLLM;
 
 internal static class Constants {
-	//internal static string _preBuildGPT20BModelPath = $"{AppContext.BaseDirectory}..\\..\\..\\ONNX\\gpt-oss-20b";
 	internal static string _gemma27BModelPath = $"{AppContext.BaseDirectory}..\\..\\..\\ONNX\\med-gemma-27B";
 
 	#region MedGemma-27B relevant
